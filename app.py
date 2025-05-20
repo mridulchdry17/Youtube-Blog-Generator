18d3889c90ddb5029affe2f75725acd9ef4175f8
mridulchdry17 <mridul17chaudhary@gmail.com>
2025-05-20 16:55:35 +0530

    Final Deployment Code

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def generate():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
